a3fc80ab15038e0816278984c9ce64c65d5f1c5c
Salih OKUR <salih.okur@tubitak.gov.tr>
2025-09-23 20:06:27 +0300

    first commit

---
+++ src/main/java/com/salcatech/nfc_api/NfcApiApplication.java
@@ -0,0 +1,13 @@
+package com.salcatech.nfc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salcatech/nfc_api/NfcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salcatech.nfc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
